14681f6f4a3f48be61c388dc144a2350c6d0dd70
Adam Doyle <dominofoe@gmail.com>
2015-02-20 10:28:05 -0500

    Completed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 $(document).ready(function() {
 
 	
-		for(var i = 1; i < 101; i++) {
+	
+	getUserInput()
+
+	
+
+	function getUserInput() {
+		var userInput = prompt("Please choose an ending point for Fizz Buzz.");
+		userInput = parseFloat(userInput, 10)
+				
+
+		if ((isNaN(userInput)) || (userInput % 1 != 0) || (userInput === null)) {
+			alert("Please enter a whole number!");
+			getUserInput();		
+		}else{
+			fizzBuzz(userInput);
+		}		
+	}	
+
+
+	function fizzBuzz(max) {
+		
+		var i = max;
+		
+		for(var i = 1; i < max; i++) {
 			
 			if((i % 3 === 0) && (i % 5 === 0)) {
 				$("body").append("<p>FizzBuzz</p>");
@@ -18,6 +41,12 @@ $(document).ready(function() {
 			}
 
 		}
+	}
+
+	
+
+
+	
 	 
 	
 });
